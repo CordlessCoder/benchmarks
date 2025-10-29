70de2f6ffc002f347c7b38aab56baf504cacbe69
CordlessCoder <roman@vm.net.ua>
2025-10-29 01:28:45 +0000

    Add sfence instructions to operations that make use of non-temporal intrinsics

--- benchmarks-memory/src/strategies.rs
+++ benchmarks-memory/src/strategies.rs
@@ -76,21 +76,30 @@ impl OperationStrategy {
             Int128 => read_by::<16, u128>,
             #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
             SSE => |data| {
+                unsafe {
+                    x86::_mm_sfence();
+                }
                 for_each_aligned_value::<32, x86::__m128i, x86::__m128i>(data, |val| unsafe {
                     x86::_mm_stream_load_si128(val.as_ptr())
-                })
+                });
             },
             #[cfg(target_arch = "x86_64")]
             AVX2 => |data| {
+                unsafe {
+                    x86::_mm_sfence();
+                }
                 for_each_aligned_value::<64, x86::__m256i, x86::__m256i>(data, |val| unsafe {
                     x86::_mm256_stream_load_si256(val.as_ptr())
-                })
+                });
             },
             #[cfg(target_arch = "x86_64")]
             AVX512 => |data| {
+                unsafe {
+                    x86::_mm_sfence();
+                }
                 for_each_aligned_value::<64, x86::__m512i, x86::__m512i>(data, |val| unsafe {
                     x86::_mm512_stream_load_si512(val.as_ptr())
-                })
+                });
             },
         }
     }
@@ -112,21 +121,24 @@ impl OperationStrategy {
                 let value = x86::_mm_set1_epi8(0xAA_u8 as i8);
                 for_each_aligned_value::<32, x86::__m128i, ()>(data, |slot| {
                     x86::_mm_stream_si128(slot.as_mut_ptr(), value)
-                })
+                });
+                x86::_mm_sfence();
             },
             #[cfg(target_arch = "x86_64")]
             AVX2 => |data| unsafe {
                 let value = x86::_mm256_set1_epi8(0xAA_u8 as i8);
                 for_each_aligned_value::<64, x86::__m256i, ()>(data, |slot| {
                     x86::_mm256_stream_si256(slot.as_mut_ptr(), value);
-                })
+                });
+                x86::_mm_sfence();
             },
             #[cfg(target_arch = "x86_64")]
             AVX512 => |data| unsafe {
                 let value = x86::_mm512_set1_epi8(0xAA_u8 as i8);
                 for_each_aligned_value::<64, x86::__m512i, ()>(data, |slot| {
                     x86::_mm512_stream_si512(slot.as_mut_ptr(), value);
-                })
+                });
+                x86::_mm_sfence();
             },
         }
     }
@@ -172,32 +184,38 @@ impl OperationStrategy {
             #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
             SSE => |from, to, len| unsafe {
                 type Register = x86::__m128i;
+                x86::_mm_sfence();
                 for_each_idx_chunked::<32, ()>(len / size_of::<Register>(), |idx| {
                     let from = from.cast::<Register>().add(idx);
                     let to = to.cast::<Register>().add(idx);
                     let val = x86::_mm_stream_load_si128(from);
                     x86::_mm_stream_si128(to, val);
                 });
+                x86::_mm_sfence();
             },
             #[cfg(target_arch = "x86_64")]
             AVX2 => |from, to, len| unsafe {
                 type Register = x86::__m256i;
+                x86::_mm_sfence();
                 for_each_idx_chunked::<64, ()>(len / size_of::<Register>(), |idx| {
                     let from = from.cast::<Register>().add(idx);
                     let to = to.cast::<Register>().add(idx);
                     let val = x86::_mm256_stream_load_si256(from);
                     x86::_mm256_stream_si256(to, val);
                 });
+                x86::_mm_sfence();
             },
             #[cfg(target_arch = "x86_64")]
             AVX512 => |from, to, len| unsafe {
                 type Register = x86::__m512i;
+                x86::_mm_sfence();
                 for_each_idx_chunked::<64, ()>(len / size_of::<Register>(), |idx| {
                     let from = from.cast::<Register>().add(idx);
                     let to = to.cast::<Register>().add(idx);
                     let val = x86::_mm512_stream_load_si512(from);
                     x86::_mm512_stream_si512(to, val);
                 });
+                x86::_mm_sfence();
             },
         }
     }
