11473ef9ffc5143ededb072d52bedb820691a8ac
CordlessCoder <roman@vm.net.ua>
2025-10-29 20:06:10 +0000

    Make benchmark selector toggleable

--- benchmarks-gui/src/main.rs
+++ benchmarks-gui/src/main.rs
@@ -21,6 +21,7 @@ fn main() -> eframe::Result {
 
 struct MyApp {
     benchmarks: Vec<Box<dyn Benchmark>>,
+    selector_panel_open: bool,
     selected_benchmark_idx: Option<usize>,
 }
 
@@ -32,6 +33,7 @@ impl MyApp {
                 Box::new(MemoryThroughputPanel::default()),
             ],
             selected_benchmark_idx: Some(0),
+            selector_panel_open: true,
         }
     }
 }
@@ -45,24 +47,28 @@ impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::TopBottomPanel::bottom("Render stats").show(ctx, |ui| {
             ui.horizontal_wrapped(|ui| {
+                egui::widgets::global_theme_preference_switch(ui);
+                ui.separator();
+                ui.toggle_value(&mut self.selector_panel_open, "Benchmark List");
+                ui.separator();
                 ui.label(format!(
                     "Render time: {:.2}ms",
                     frame.info().cpu_usage.unwrap_or_default() * 1000.0
                 ));
-                ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                    egui::widgets::global_theme_preference_switch(ui);
-                })
             })
         });
-        egui::SidePanel::left("Benchmark selector").show(ctx, |ui| {
-            for (idx, benchmark) in self.benchmarks.iter().enumerate() {
-                ui.selectable_value(
-                    &mut self.selected_benchmark_idx,
-                    Some(idx),
-                    benchmark.name(),
-                );
-            }
-        });
+        egui::SidePanel::left("Benchmark selector")
+            .resizable(false)
+            .show_animated(ctx, self.selector_panel_open, |ui| {
+                ui.add_space(ui.spacing().menu_margin.topf());
+                for (idx, benchmark) in self.benchmarks.iter().enumerate() {
+                    ui.selectable_value(
+                        &mut self.selected_benchmark_idx,
+                        Some(idx),
+                        benchmark.name(),
+                    );
+                }
+            });
         egui::CentralPanel::default().show(ctx, |ui| {
             egui::ScrollArea::vertical().show(ui, |ui| {
                 ui.take_available_space();
